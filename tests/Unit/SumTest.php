25fde8fb242b9f13a02bdabb346243b2d7fac358
Maryam Mohamed Auf <maryammohamedauf@gmail.com>
2024-12-08 09:45:35 +0200

    Try more forms of Pest testing

--- tests/Unit/SumTest.php
+++ tests/Unit/SumTest.php
@@ -3,17 +3,32 @@
 function sum($a, $b) {
     return $a + $b;
 }
-
 test('sum', function () {
    $result = sum(1, 2);
  
    expect($result)->toBe(3);
 });
-
 it('performs sums', function () {
     $result = sum(1, 2);
   
     expect($result)->toBe(3);
  });
- 
+ describe('sum', function () {
+    it('may sum integers', function () {
+        $result = sum(1, 2);
+  
+        expect($result)->toBe(3);
+     });
+  
+     it('may sum floats', function () {
+        $result = sum(1.5, 2.5);
+  
+        expect($result)->toBe(4.0);
+     });
+ });
+ test('sumCalc', function () {
+    $result = sum(1, 2);
+  
+    $this->assertSame(3, $result); // Same as expect($result)->toBe(3)
+ });
 ?>
