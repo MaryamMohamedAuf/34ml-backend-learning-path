050de105bda4a81112e0a04c2fedb124413c9629
Maryam Mohamed Auf <maryammohamedauf@gmail.com>
2024-12-08 20:58:52 +0200

    Compare the performance of queries with lazy loading vs. eager loading

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,7 +10,16 @@ class StudentController extends Controller
 {
     public function index()
     {
-       
+        $students = Student::all(); // Fetch all students (1 query)
+foreach ($students as $student) {
+    $comments = $student->comments; // Query for each student's comments
+    echo $student->name . "'s Comments:\n";
+    foreach ($comments as $comment) {
+        echo "- " . $comment->content . "\n";
+    }
+}
+
+
        $students = Student::with('comments')->get(); //Eager Loading
     
     foreach ($students as $student) {
